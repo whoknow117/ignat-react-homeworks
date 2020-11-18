755c2c3f52ffd27fb11fce234007889306a32061
newbeeguy <mskjavascript@gmail.com>
2020-11-18 20:21:46 +0300

    Updates

---
+++ precache-manifest.bf2d7ef768d285834f5c7a453b83353e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0dc8d3da67aabaf6e9b888610cc4ad",
+    "url": "/ignat-react-homeworks/index.html"
+  },
+  {
+    "revision": "0d0b43f1540d6763ca74",
+    "url": "/ignat-react-homeworks/static/css/main.730a4ad5.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/ignat-react-homeworks/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ignat-react-homeworks/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0b43f1540d6763ca74",
+    "url": "/ignat-react-homeworks/static/js/main.53e707b8.chunk.js"
+  },
+  {
+    "revision": "011c718218a22c1587d6",
+    "url": "/ignat-react-homeworks/static/js/runtime-main.e9a55379.js"
+  }
+]);
